441ba3a94335f6141443613d8d090d16e0e91949
MamFrey <freyjennygragasin@gmail.com>
2026-07-20 17:20:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Mobile Menu
 const menuToggle =
 document.querySelector(".menu-toggle");
 
@@ -5,7 +6,37 @@ const navLinks =
 document.querySelector(".nav-links");
 
 menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
 
-navLinks.classList.toggle("active");
 
+// ===========================
+// GOOGLE SHEETS ANNOUNCEMENTS
+// ===========================
+
+const url = "https://script.google.com/macros/s/AKfycbx2w8N9bjYUgTXAcgf11kXPu9ZJBIEL9ekSeQI8cYMzM26lvWqCYiFKrSEjlGA1FurM7g/exec";
+
+fetch(url)
+.then(response => response.json())
+.then(data => {
+
+    let html = "";
+
+    data.forEach(item => {
+
+        html += `
+            <div class="announcement-card">
+                <h3>${item.title}</h3>
+                <small>${item.date}</small>
+                <p>${item.content}</p>
+            </div>
+        `;
+
+    });
+
+    document.getElementById("announcement-list").innerHTML = html;
+
+})
+.catch(error => {
+    console.error(error);
 });
